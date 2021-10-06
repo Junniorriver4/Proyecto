1fc52a0413a3ac9766427ee7b3ded114c94f9070
junnior <71543780@continental.edu.pe>
2021-10-06 12:13:58 -0500

    subiendo los primeros archivos

---
+++ Programa1/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import polls
+
+# Create your views here.
+def index(request):
+    return render(request, 'polls/starter.html')
